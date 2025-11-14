410f665dd7b1efb89bb55e62f292944a60824d6c
Enric Domingo <enric.domingo@gmail.com>
2025-11-14 11:44:43 +0000

    Add Docstring

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,3 +1,6 @@
+
+# """ Climate Analysis Tools """
+
 import sys
 import temp_conversion
 import signal
@@ -25,3 +28,4 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
